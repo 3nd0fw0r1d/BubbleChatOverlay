1b3ff90b4ede42ee82b6e094320f3ccd8237ceb7
End <3nd0fwilliams@gmail.com>
2023-10-13 02:55:46 -0400

    Update widget.js

--- widget.js
+++ widget.js
@@ -4,11 +4,14 @@ let totalMessages = 0,
     channelName = "testchannel",
     provider = "twitch",
     animationIn = "fadeInUp",
-    animationOut = "bounceOut";
+    animationOut = "bounceOut",
+    smallDelay = 250,
+    badgesEnable = "yes";
 
 window.addEventListener("onWidgetLoad", function (obj) {
    const fieldData = obj.detail.fieldData;
    animationIn = fieldData.animationIn;
+   badgesEnable = fieldData.badgesEnable;
    channelName = obj.detail.channel.username;
    ignoredUsers = fieldData.ignoredUsers.toLowerCase().replace(" ", "").split(",");
 });
@@ -131,14 +134,16 @@ window.addEventListener('onEventReceived', function (stream_event) {
     username_box.style.backgroundColor = user_color; // set the username box color as the twitch username color
     // typically the CSS is used to define the static colors for things otherwise
 
-    for (let i = 0; i < data.badges.length; i++) { // so prior to the username, the user badges...
-        let badge = data.badges[i];
-        let badge_elem = document.createElement("img");
-        badge_elem.className = "badge";
-        badge_elem.alt= "";
-        badge_elem.src = badge.url;
+    if (badgesEnable === "yes") {
+        for (let i = 0; i < data.badges.length; i++) { // so prior to the username, the user badges...
+            let badge = data.badges[i];
+            let badge_elem = document.createElement("img");
+            badge_elem.className = "badge";
+            badge_elem.alt= "";
+            badge_elem.src = badge.url;
 
-        username_box.appendChild(badge_elem);
+            username_box.appendChild(badge_elem);
+        }
     }
 
     let username_elem = document.createElement("span");
@@ -181,71 +186,76 @@ window.addEventListener('onEventReceived', function (stream_event) {
     let list_container = document.getElementsByClassName("main-container")[0];
 
     list_container.appendChild(message_container);
+  
+    let this_direction = current_username_bias;
+    
+    if (current_username_bias === "right") {
+      current_username_bias = "left";
+    } else {
+      current_username_bias = "right";
+    }
 
     // EVERYTHING BELOW HERE IS TO ADD THE BUBBLES AND SHADOW
     // SINCE IT HAS TO BE ON SCREEN TO MAKE SURE THE RECT SIZE IS RIGHT
-    const rect = user_message.getBoundingClientRect();
-    const xy_arr = [ // position of all the bubbles :)
-        [36, 8],
-        [-4, Math.floor(rect.height / 2)],
-        [rect.width - 4, Math.floor(rect.height / 2) - 2],
-        [rect.width - 35, rect.height - 4],
-        [rect.width - 66, rect.height - 12],
-        [rect.width - 58, rect.height + 10],
-        [rect.width - 42, rect.height + 18]
-    ];
-    const size_arr = [44, 20, 28, 30, 26, 16, 10]; // size of all the bubbles
+    setTimeout(function(){
+      const rect = user_message.getBoundingClientRect();
+      const xy_arr = [ // position of all the bubbles :)
+          [36, 8],
+          [-4, Math.floor(rect.height / 2)],
+          [rect.width - 4, Math.floor(rect.height / 2) - 2],
+          [rect.width - 35, rect.height - 4],
+          [rect.width - 66, rect.height - 12],
+          [rect.width - 58, rect.height + 10],
+          [rect.width - 42, rect.height + 18]
+      ];
+      const size_arr = [44, 20, 28, 30, 26, 16, 10]; // size of all the bubbles
 
-    if (data.channel !== channelName) {
-        for (let i = 0; i < xy_arr.length; i++) { // background bubbles
-            let x_off = Math.floor(xy_arr[i][0] - (size_arr[i] / 2)) + 12; // manual offsets also because why not
-            let y_off = Math.floor(xy_arr[i][1] - (size_arr[i] / 2)) + 36 - 16; // the -16 is to match the CSS offset
-            x_off -= 4;
-            y_off += 4; // then offset diagonally since these are the shadows
+      if (data.channel !== channelName) {
+          for (let i = 0; i < xy_arr.length; i++) { // background bubbles
+              let x_off = Math.floor(xy_arr[i][0] - (size_arr[i] / 2)) + 12; // manual offsets also because why not
+              let y_off = Math.floor(xy_arr[i][1] - (size_arr[i] / 2)) + 36 - 16; // the -16 is to match the CSS offset
+              x_off -= 4;
+              y_off += 4; // then offset diagonally since these are the shadows
 
-            let test = document.createElement("div");
-            test.className = "bubble";
-            test.style.backgroundColor = user_color;
-            test.style.transform = `translate(${x_off}px, ${y_off}px)`;
-            test.style.width = `${size_arr[i]}px`;
-            test.style.height = `${size_arr[i]}px`;
+              let test = document.createElement("div");
+              test.className = "bubble";
+              test.style.backgroundColor = user_color;
+              test.style.transform = `translate(${x_off}px, ${y_off}px)`;
+              test.style.width = `${size_arr[i]}px`;
+              test.style.height = `${size_arr[i]}px`;
 
-            message_container_inner.appendChild(test);
-        }
-    }
+              message_container_inner.appendChild(test);
+          }
+      }
 
-    let shadow_elem = document.createElement("div");
-    shadow_elem.className = "message-shadow";
-    shadow_elem.style.width = `${rect.width}px`; // an actual drop shadow in CSS ends up in front of the bubbles
-    shadow_elem.style.height = `${rect.height}px`; // hence this jank
-    shadow_elem.style.backgroundColor = user_color;
+      let shadow_elem = document.createElement("div");
+      shadow_elem.className = "message-shadow";
+      shadow_elem.style.width = `${rect.width}px`; // an actual drop shadow in CSS ends up in front of the bubbles
+      shadow_elem.style.height = `${rect.height}px`; // hence this jank
+      shadow_elem.style.backgroundColor = user_color;
 
-    message_container_inner.appendChild(shadow_elem);
+      message_container_inner.appendChild(shadow_elem);
 
-    if (data.channel !== channelName) {
-        for (let i = 0; i < xy_arr.length; i++) { // foreground bubbles
-            let x_off = Math.floor(xy_arr[i][0] - (size_arr[i] / 2)) + 12;
-            let y_off = Math.floor(xy_arr[i][1] - (size_arr[i] / 2)) + 36 - 16;
+      if (data.channel !== channelName) {
+          for (let i = 0; i < xy_arr.length; i++) { // foreground bubbles
+              let x_off = Math.floor(xy_arr[i][0] - (size_arr[i] / 2)) + 12;
+              let y_off = Math.floor(xy_arr[i][1] - (size_arr[i] / 2)) + 36 - 16;
 
-            let test = document.createElement("div");
-            test.className = "bubble";
-            test.style.transform = `translate(${x_off}px, ${y_off}px)`;
-            test.style.width = `${size_arr[i]}px`;
-            test.style.height = `${size_arr[i]}px`;
+              let test = document.createElement("div");
+              test.className = "bubble";
+              test.style.transform = `translate(${x_off}px, ${y_off}px)`;
+              test.style.width = `${size_arr[i]}px`;
+              test.style.height = `${size_arr[i]}px`;
 
-            message_container_inner.appendChild(test);
-        }
-    }
+              message_container_inner.appendChild(test);
+          }
+      }
 
-    message_container_inner_pre.style.transform = current_username_bias === "left" ? "rotate(-3deg)" : "rotate(3deg)";
+      message_container_inner_pre.style.transform = this_direction === "left" ? "rotate(-3deg)" : "rotate(3deg)";
+
+    }, smallDelay);
   
-    if (current_username_bias === "right") {
-        current_username_bias = "left";
-    } else {
-        current_username_bias = "right";
-    }
-  
-  	let main_spacer = document.getElementsByClassName("main-spacer")[0];
+    let main_spacer = document.getElementsByClassName("main-spacer")[0];
     while (main_spacer.clientHeight == 0) {
         list_container.removeChild(list_container.childNodes[2]);
     }
